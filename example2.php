1a747b6b958b19acf5f4ebd0e2fe4feada861693
Simon Rubuano <contact@mgkprod.fr>
2018-07-05 11:35:40 +0200

    Initial commit

---
+++ example2.php
@@ -0,0 +1,41 @@
+<?php
+
+use Payline\PaylineSDK;
+
+require_once 'vendor/autoload.php';
+include "config.php";
+include "include.php";
+
+$paylineSDK = new PaylineSDK($PAYLINE_MERCHANT_ID, $PAYLINE_ACCESS_KEY, '', '', '', '', PaylineSDK::ENV_HOMO);
+$payment_id = 'inv_test3x_' . uniqid();
+
+$doWebPayment = [
+    'version' => 17,
+    'returnURL' => 'https://website.com/payline/callback',
+    'cancelURL' => 'https://website.com/payline/callback/cancel',
+    'selectedContractList' => $PAYLINE_CONTRACT_NUMBER,
+    'secondSelectedContractList' => $PAYLINE_3X_CONTRACT_NUMBER,
+];
+
+$doWebPayment['payment'] = [
+    'amount' => (int) 5000,
+    'currency' => 978,
+    'action' => 101,
+    'mode' => 'CPT',
+    'contractNumber' => $PAYLINE_CONTRACT_NUMBER,
+];
+
+$doWebPayment['order'] = [
+    'ref' => 'ref',
+    'country' => 'FR',
+    'amount' => 5000,
+    'currency' => 978,
+    'date' => date('d/m/Y H:i'),
+    'deliveryMode' => 4,
+];
+
+dump($doWebPayment);
+
+$doWebPaymentResponse = $paylineSDK->doWebPayment($doWebPayment);
+
+dd($doWebPaymentResponse);
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+use Payline\PaylineSDK;
+
+require_once 'vendor/autoload.php';
+include "config.php";
+include "include.php";
+
+?>
+
+<a href="example1.php">doWebPayment, without "version"</a><br>
+<a href="example2.php">doWebPayment, with "version"</a>
