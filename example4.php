bf044d7f2717fd559890f935c928cb7954fa5c93
Simon Rubuano <contact@mgkprod.fr>
2018-07-17 10:58:08 +0200

    Added example4

---
+++ example4.php
@@ -0,0 +1,59 @@
+<?php
+
+use Payline\PaylineSDK;
+
+require_once 'vendor/autoload.php';
+include "config.php";
+include "include.php";
+
+$paylineSDK = new PaylineSDK($PAYLINE_MERCHANT_ID, $PAYLINE_ACCESS_KEY, '', '', '', '', PaylineSDK::ENV_HOMO);
+$payment_id = 'inv_test3x_' . uniqid();
+
+$doWebPayment = [
+    'version' => 17,
+    'returnURL' => 'https://website.com/payline/callback',
+    'cancelURL' => 'https://website.com/payline/callback/cancel',
+];
+
+$doWebPayment['contracts'] = [$PAYLINE_3X_CONTRACT_NUMBER];
+
+$doWebPayment['payment'] = [
+    'amount' => (int) 50000,
+    'currency' => 978,
+    'action' => 101,
+    'mode' => 'CPT',
+    'contractNumber' => $PAYLINE_3X_CONTRACT_NUMBER,
+];
+
+$doWebPayment['order'] = [
+    'ref' => $payment_id,
+    'country' => 'FR',
+    'amount' => 50000,
+    'currency' => 978,
+    'date' => date('d/m/Y H:i'),
+    'deliveryMode' => 4,
+];
+
+$doWebPayment['buyer'] = [
+    'title' => 'MR',
+    'firstName' => 'ERIC',
+    'lastName' => 'DUPONT',
+    'mobilePhone' => '0654874564',
+    'email' => 'eric.dupont@email.com',
+    'billingAddress' => [
+        'country' => 'FRANCE',
+        'street1' => '72 RUE MICHEL ANGE',
+        'zipCode' => '75016',
+        'cityName' => 'PARIS',
+    ],
+];
+
+dump('SDK request:', $doWebPayment);
+
+$doWebPaymentResponse = $paylineSDK->doWebPayment($doWebPayment);
+
+dump('WS response:', $doWebPaymentResponse);
+
+if (isset($doWebPaymentResponse['redirectURL'])) {
+    echo 'Success !<br><a href="' . $doWebPaymentResponse["redirectURL"] . '">' . $doWebPaymentResponse["redirectURL"] . '</a>';
+}
--- index.php
+++ index.php
@@ -10,4 +10,5 @@ include "include.php";
 
 <s><a href="example1.php">Failed doWebPayment, without "version"</a></s><br>
 <s><a href="example2.php">Failed doWebPayment, with "version"</a></s><br>
-<a href="example3.php">Working doWebPayment, with buyerAddress inconsistency and Cofidis timeout</a>
+<a href="example3.php">Working doWebPayment (with buyerAddress on root level), Cofidis timeout</a><br>
+<a href="example4.php">Failed doWebPayment (with buyerAddress inconsistency)</a>
